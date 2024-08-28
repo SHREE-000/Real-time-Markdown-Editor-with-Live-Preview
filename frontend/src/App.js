eb7187970ac9f81fb46d0207984adcc70668f4c2
Shreehari C R <shreehari000@gamil.com>
2024-08-28 08:33:25 +0530

    added frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MarkdownEditor from './MarkdownEditor';
+
+function App() {
+  
+  return (
+    <div>
+      <h1 style={{textAlign: 'center', marginTop: '10px'}}>Real-time Markdown Editor</h1>
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+export default App;
